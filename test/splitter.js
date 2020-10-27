bb51c47a31429bab9f39136b73052b76323f3549
0surface <tms.0xspin@gmail.com>
2020-10-27 17:03:41 +0000

    fixed typo in test file

--- test/splitter.js
+++ test/splitter.js
@@ -1,9 +1,9 @@
-const splitterContract = artifacts.require("Splitter");
+const Splitter = artifacts.require("Splitter");
 
 //remove this
 console.log(web3.currentProvider);
 
-splitterContract("Splitter", (accounts) => {
+contract("Splitter", (accounts) => {
   it("", () => {
     const testNum = 42;
     assert.equal(testNum, 42, "Test works");
