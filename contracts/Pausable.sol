52a00bbc60ed270cc5035bd510529df523c42fca
0surface <tms.0xvoid@gmail.com>
2020-11-05 18:13:56 +0000

    add Pausable contract

---
+++ contracts/Pausable.sol
@@ -0,0 +1,44 @@
+pragma solidity 0.5.16;
+
+import "./Owned.sol";
+
+contract Pausable is Owned {
+    
+    event LogContractPaused();
+    event LogContractUnpaused();
+
+  bool public paused = false;
+
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is not paused.
+   */
+  modifier whenNotPaused() {
+    require(!paused, "Contract is paused");
+    _;
+  }
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is paused.
+   */
+  modifier whenPaused() {
+    require(paused, "Contract is not paused");
+    _;
+  }
+
+  /**
+   * @dev called by the owner to pause, triggers stopped state
+   */
+  function pause() onlyOwner whenNotPaused public {
+    paused = true;
+    emit LogContractPaused();
+  }
+
+  /**
+   * @dev called by the owner to unpause, returns to normal state
+   */
+  function unpause() onlyOwner whenPaused public {
+    paused = false;
+    emit LogContractUnpaused();
+  }
+}
